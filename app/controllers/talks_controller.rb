764036cf80ebaa1796e0e9ff40539ea7c77952d7
kenhakucho <bagle0309@gmail>
2016-06-24 12:05:10 +0000

    torkun

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -3,7 +3,7 @@ class TalksController < ApplicationController
   end
   
   def callback
-   if params["hub.verify_token"] == "1150492266"
+   if params["hub.verify_token"] == "56126624"
       render json: params["hub.challenge"]
    else
       render json: "Error, wrong validation token"
