37888720b7ebc81fc14a0cb7e7565a7a137c850c
kenhakucho <bagle0309@gmail>
2016-06-25 07:37:03 +0000

    mimi

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,27 +1,20 @@
 class TalksController < ApplicationController
-  def new
-  end
-  
+
+  require "json"
+#  require "rest-client"
+
   # def callback
-  #   token = "EAAI9422tpG8BAFrKuTdGvZA4yKSmLCHwp6O0KI4vYEcmHqwhuKyMTwKR4C9ZCMuvLmRN8I3qsU5SmalrqPx1HFZArl2yToNA81VEWSDumPlNZBEtLTiOeuLQZClXOnw5wuqKZAGYIIL01L0WliMz3TFMGAt7l890XhU6V1zDT9qwZDZD"
-  #   logger.info("callback start ***************************************")
-  #   logger.info(params)
   #   p params
   #   message = params["entry"][0]["messaging"][0] #unless !params.present? || !params["entry"].present?
-
   #   if message.include?("message")
-
   #     #ユーザーの発言
   #     sender = message["sender"]["id"]
   #     text = message["message"]["text"]
-
   #     endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
   #     request_content = {recipient: {id:sender},
   #                       message: {text: text}
   #                       }
-
   #     content_json = request_content.to_json
-
   #       RestClient.post(endpoint_uri, content_json, {
   #         'Content-Type' => 'application/json; charset=UTF-8'
   #         }){ |response, request, result, &block|
@@ -34,24 +27,27 @@ class TalksController < ApplicationController
   #   end
   # end
 
-require "json"
-require 'rest-client'
+  def callback
+    # request_body = JSON.parse(request.body.read) 
+    if request.body.read.present?
+      logger.info("--------------------------------------------")
+      logger.info(request.body.read)
+      
+      request_body ||= JSON.parse(request.body.read, {:symbolize_names => true}) 
+      messaging_events = request_body["entry"][0]["messaging"]
 
-  post "callback" do
-    
-    request_body = JSON.parse(request.body.read)
-    messaging_events = request_body["entry"][0]["messaging"]
-    messaging_events.each do |event|
-      sender = event["sender"]["id"]
+      messaging_events.each do |event|
+        sender = event["sender"]["id"]
   
-      if !event["message"].nil? && !event["message"]["text"].nil?
-        text = event["message"]["text"]
-        bot_response(sender, text)
+        if !event["message"].nil? && !event["message"]["text"].nil?
+          text = event["message"]["text"]
+          bot_response(sender, text)
+        end
       end
-    end
-  
-    status 201
-    body ''
+      status 201
+      body ''
+    
+    end 
   end
   
   def bot_response(sender, text)
@@ -71,5 +67,4 @@ require 'rest-client'
       }
     }.to_json
   end
-
 end
