1a5f2e3411aac99a77f55801de1e96522fd06c8a
kenhakucho <bagle0309@gmail>
2016-06-25 09:54:55 +0000

    logs fix

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -10,7 +10,7 @@ class TalksController < ApplicationController
       messaging_events = request_body["entry"][0]["messaging"]
 
       logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-      logger.info("messaging_events : #{messaging_events.count}")
+      logger.info("messaging_events : #{messaging_events}")
       logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       
       messaging_events.each do |event|
@@ -18,11 +18,6 @@ class TalksController < ApplicationController
         
         if !event["message"].nil? && !event["message"]["text"].nil?
           text = event["message"]["text"]
-
-          logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-          logger.info(p text)
-          logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
           bot_response(sender, text)
         end
       end
