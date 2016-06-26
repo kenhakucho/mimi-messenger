8de3c31d650bd0dd8ab50c639e661138bc2e9d4d
kenhakucho <bagle0309@gmail>
2016-06-26 01:04:06 +0000

    oremo orm

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,4 +1,6 @@
 class TalksController < ApplicationController
+  require "json"
+  require "rest-client"
   protect_from_forgery except: :callback
 
   def callback
@@ -6,14 +8,15 @@ class TalksController < ApplicationController
     
     if message.include?("message")
       sender = message["sender"]["id"]
-      text = message["message"]["text"]
-
+      text = message["message"]["text"] + "¥r¥nおれも"
       endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV["FACEBOOK_PAGE_TOKEN"]}" 
       request_content = {recipient: {id:sender},
                          message: {text: text}
                         }
       content_json = request_content.to_json
-
+      
+#      result = bot_response(sender, text)
+#      RestClient.post(endpoint_uri, result, {
       RestClient.post(endpoint_uri, content_json, {
         'Content-Type' => 'application/json; charset=UTF-8'
       }){ |response, request, result, &block|
@@ -26,42 +29,41 @@ class TalksController < ApplicationController
     end
   end
   
-  def test
-    if request.body.read.present?
-      request_body = JSON.parse(request.body.read) 
-      logger.info("request_body : #{request_body}")
-      messaging_events = request_body["entry"][0]["messaging"]
-      messaging_events.each do |event|
-        sender = event["sender"]["id"]
-        if !event["message"].nil? && !event["message"]["text"].nil?
-          text = event["message"]["text"]
-          bot_response(sender, text)
-        end
-      end
-
-      logger.info("e"*20)
-      status 201
-      body ''
-    end 
-  end
-  
   def bot_response(sender, text)
-    request_endpoint = "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV["FACEBOOK_PAGE_TOKEN"]}"
+    clientid = "VBVBSDOsTPSDxQpsJ6Z95MzSSrKwBp9jSgVn7Ba0G1ve"
+    clientsecret="(~ZWr6mKS7<M2KX+&OmE"
+    apikey="32566a494a756556334362627046676479442e6d636556352f6a71756a4c774d5a706c6f4e756b64334741"
+    request_endpoint = "https://api.apigw.smt.docomo.ne.jp/scenarioDialogue/v1/registration?APIKEY=#{apikey}"
 
-    logger.info("1"*20)
-    request_body = text_message_request_body(sender, text)
-    logger.info("w"*20)
-    RestClient.post request_endpoint, request_body, content_type: :json, accept: :json
-  end
-  
-  def text_message_request_body(sender, text)
-    {
-      recipient: {
-        id: sender
-      },
-      message: {
-        text: text
-      }
-    }.to_json
+    request_body = {'botId' => 'APIBot'}
+    
+    appid = RestClient.post request_endpoint, request_body, content_type: :json, accept: :json 
+
+    res = Net::HTTP.post_form(endpoint, request_body)
+#    Net::HTTP.post_form(URI.parse(request_endpoint), request_body)
+
+    # request_body = {
+　　  # 'appId' => appid,
+　　  # 'botId' => "APIBot",
+　　  # 'voiceText' => text.presence || "init",
+　　  # 'initTalkingFlag' => "true",
+　　  # 'initTopicId' => "APITOPIC"
+#      'appRecvTime' => "2016-6-25 19:52:31",
+#      'appSendTime' => "2016-6-25 19:52:31" 
+　　# }
+
+    appid = RestClient.post request_endpoint, request_body, content_type: :json, accept: :json
+
+#    request_endpoint = URI.parse("https://api.apigw.smt.docomo.ne.jp/scenarioDialogue/v1/dialogue?APIKEY=#{apikey}")"
+　　
+      # [:systemText][:expression]
+      {
+        recipient: {
+          id: sender
+        },
+        message: {
+          text: text
+        }
+      }.to_json
   end
 end
